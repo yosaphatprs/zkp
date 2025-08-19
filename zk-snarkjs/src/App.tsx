b3b49727487b78b05ac794cc74277d69fa0efd9a
yosaphatprs <susilojosafat@gmail.com>
2025-08-19 15:34:52 +0700

    feat: add input and tidy FE for preview

--- zk-snarkjs/src/App.tsx
+++ zk-snarkjs/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 declare global {
@@ -7,54 +8,111 @@ declare global {
 }
 
 function App() {
-  const proofComponent = document.getElementById("proof");
-  const resultComponent = document.getElementById("result");
-  const bGenProof = document.getElementById("bGenProof");
   const snarkjs = window.snarkjs;
+  const [proof, setProof] = useState("");
+  const [publicSign, setPublicSign] = useState("");
+  const [verifyValue, setVerifyValue] = useState("none");
+  const [isLoading, setIsLoading] = useState(false);
+  // const [proofCounter, setProofCounter] = useState(0);
 
-  if (bGenProof) {
-    bGenProof.addEventListener("click", calculateProof);
+  const initialInput = {
+    inputA: "",
+    inputB: "",
+  };
+
+  const [inputFields, setInput] = useState(initialInput);
+  const { inputA, inputB } = inputFields;
+
+  function handleChange(e: any) {
+    const { name, value } = e.target;
+    setInput(() => {
+      return {
+        ...inputFields,
+        [name]: value,
+      };
+    });
   }
 
   async function calculateProof() {
+    setIsLoading(true);
     console.log("Calculating Proof");
-    const a = 1;
-    const b = 15;
+    // setProofCounter((c) => {
+    //   if (c >= 20) {
+    //     alert(
+    //       "Silahkan refresh halaman ini, karena kebutuhan memory sudah terlalu besar!"
+    //     );
+    //     console.log("Refresh this page");
+    //   }
+    //   return c + 1;
+    // });
+    // setProofCounter(proofCounter + 1);
     const { proof, publicSignals } = await snarkjs.groth16.fullProve(
-      { a: a, b: b },
+      { a: inputA, b: inputB },
       "circuit.wasm",
       "circuit_final.zkey"
     );
-    console.log(publicSignals);
-    console.log(a, b);
-    if (proofComponent) {
-      proofComponent.innerHTML = JSON.stringify(proof, null, 1);
-    }
+    setProof(JSON.stringify(proof, null, 1));
+    setPublicSign(publicSignals);
 
     const vkey = await fetch("verification_key.json").then(function (res) {
       return res.json();
     });
 
     const res = await snarkjs.groth16.verify(vkey, publicSignals, proof);
-
-    if (resultComponent) {
-      resultComponent.innerHTML = res;
-    }
+    setVerifyValue(res.toString());
+    setIsLoading(false);
   }
 
   return (
     <>
       <h1>Snarkjs client example</h1>
-      <button id="bGenProof" onClick={calculateProof}>
-        Create proof
+      <code style={{ display: "block", marginBottom: "12px" }}>
+        Input bellow is based from the circuit configured on Snarkjs part,
+        including how much input should be here and what type it was.
+        <br></br>
+        This example use two integer inputs.
+      </code>
+      <input
+        name="inputA"
+        value={inputA}
+        onChange={handleChange}
+        style={{
+          display: "block",
+          padding: "4px",
+          marginTop: "8px",
+          marginBottom: "8px",
+        }}
+      ></input>
+      <input
+        name="inputB"
+        value={inputB}
+        onChange={handleChange}
+        style={{
+          display: "block",
+          padding: "4px",
+          marginTop: "8px",
+          marginBottom: "8px",
+        }}
+      ></input>
+      <button
+        id="bGenProof"
+        onClick={calculateProof}
+        disabled={isLoading}
+        style={isLoading ? { cursor: "not-allowed" } : {}}
+      >
+        {isLoading ? "Loading..." : "Create Proof"}
       </button>
 
       <pre className="proof">
-        Proof: <code id="proof"></code>
+        Proof: <code id="proof">{proof}</code>
       </pre>
 
-      <pre className="proof">
-        Result: <code id="result"></code>
+      <pre className="publicSign">
+        Public Sign: <code id="publicSign">{publicSign}</code>
+      </pre>
+
+      <pre className="result">
+        Result: <code id="result">{verifyValue}</code>
       </pre>
     </>
   );
