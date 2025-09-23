5b949b625c260fe7c5eab9f75c6c808a89e1da1e
yosaphatprs <susilojosafat@gmail.com>
2025-09-23 17:10:53 +0700

    img: add benchmark IMG result

--- benchmark/snarkjs.sh
+++ benchmark/snarkjs.sh
@@ -6,7 +6,7 @@ cd ../snarkjs-medrec
 # Compute Witness
 hyperfine --runs 100 --warmup 5 "snarkjs wtns calculate circuit_js/circuit.wasm ../circuit-snarkjs-medrec/input_no_merkle.json witness.wtns" --export-json compute-witness.json 
 ## Verify Witness
-snarkjs wtns check circuit.r1cs witness.wtns
+# snarkjs wtns check circuit.r1cs witness.wtns
 
 # Generate Proof
 hyperfine --runs 100 --warmup 5 "snarkjs groth16 prove circuit_final.zkey witness.wtns proof.json public.json" --export-json generate-proof.json
